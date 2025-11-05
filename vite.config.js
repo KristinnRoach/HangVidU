758d51096b7ba68c0c82b457d4ebd264b872f463
kiddi <kristinnroach@gmail.com>
2025-11-05 17:41:20 +0000

    add dev:disable-pwa script (need to manually unregister SWs once when running it)

--- vite.config.js
+++ vite.config.js
@@ -6,78 +6,83 @@ import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig(({ mode }) => {
   const basePath = mode === 'production' ? '/HangVidU/' : '/';
+  const disablePWA = process.env.DISABLE_PWA === '1';
 
   return {
     base: basePath,
 
     plugins: [
       ...(mode === 'development' ? [mkcert()] : []),
-      VitePWA({
-        includeAssets: ['favicon.ico'],
-        registerType: 'autoUpdate',
-        strategies: 'generateSW',
-        injectRegister: 'script',
-        devOptions: {
-          enabled: true, // enable service worker & manifest during dev for install prompt testing
-        },
-        workbox: {
-          cleanupOutdatedCaches: true,
-        },
-        // strategies: 'injectManifest',
-        // srcDir: './service-worker',
-        // filename: 'sw.js',
-        // injectManifest: {
-        //   minify: false,
-        //   enableWorkboxModulesLogs: true,
-        // },
+      ...(disablePWA
+        ? []
+        : [
+            VitePWA({
+              includeAssets: ['favicon.ico'],
+              registerType: 'autoUpdate',
+              strategies: 'generateSW',
+              injectRegister: 'script',
+              devOptions: {
+                enabled: true, // enable service worker & manifest during dev for install prompt testing
+              },
+              workbox: {
+                cleanupOutdatedCaches: true,
+              },
+              // strategies: 'injectManifest',
+              // srcDir: './service-worker',
+              // filename: 'sw.js',
+              // injectManifest: {
+              //   minify: false,
+              //   enableWorkboxModulesLogs: true,
+              // },
 
-        manifest: {
-          name: 'HangVidU',
-          short_name: 'HangVidU',
-          description: 'Peer-to-peer video chat with watch-together mode',
+              manifest: {
+                name: 'HangVidU',
+                short_name: 'HangVidU',
+                description: 'Peer-to-peer video chat with watch-together mode',
 
-          start_url: basePath,
-          scope: basePath,
+                start_url: basePath,
+                scope: basePath,
 
-          display: 'standalone',
-          theme_color: '#82b5ecff',
-          background_color: '#1a1a1a',
+                display: 'standalone',
+                theme_color: '#82b5ecff',
+                background_color: '#1a1a1a',
 
-          icons: [
-            {
-              src: `${basePath}icons/play-arrows-v1/icon-192.png`,
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: `${basePath}icons/play-arrows-v1/icon-512.png`,
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any',
-            },
-            {
-              src: `${basePath}icons/play-arrows-v1/icon-512.png`,
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'maskable',
-            },
-          ],
-          screenshots: [
-            {
-              src: `${basePath}screenshot-wide.png`,
-              sizes: '1280x720',
-              form_factor: 'wide',
-              type: 'image/png',
-            },
-            {
-              src: `${basePath}screenshot-narrow.png`,
-              sizes: '540x720',
-              form_factor: 'narrow',
-              type: 'image/png',
-            },
-          ],
-        },
-      }),
+                icons: [
+                  {
+                    src: `${basePath}icons/play-arrows-v1/icon-192.png`,
+                    sizes: '192x192',
+                    type: 'image/png',
+                  },
+                  {
+                    src: `${basePath}icons/play-arrows-v1/icon-512.png`,
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any',
+                  },
+                  {
+                    src: `${basePath}icons/play-arrows-v1/icon-512.png`,
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'maskable',
+                  },
+                ],
+                screenshots: [
+                  {
+                    src: `${basePath}screenshot-wide.png`,
+                    sizes: '1280x720',
+                    form_factor: 'wide',
+                    type: 'image/png',
+                  },
+                  {
+                    src: `${basePath}screenshot-narrow.png`,
+                    sizes: '540x720',
+                    form_factor: 'narrow',
+                    type: 'image/png',
+                  },
+                ],
+              },
+            }),
+          ]),
     ],
     server: {
       https: true, // use trusted dev cert from mkcert
