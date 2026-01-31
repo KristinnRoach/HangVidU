dc4990741fdcbcc34bb562eb6bf033740b09deac
kiddi <kristinnroach@gmail.com>
2026-01-31 23:24:59 +0000

    Updates

--- assets/missed-call-notification-BEnGrJb5.js
+++ assets/missed-call-notification-BEnGrJb5.js
@@ -1,4 +1,4 @@
-import{c as s}from"./main-C4usWvKq.js";function f({callerId:c,callerName:l,roomId:e,onCallBack:t,onDismiss:a}){return s({template:`
+import{c as s}from"./main-j_VGhLhH.js";function f({callerId:c,callerName:l,roomId:e,onCallBack:t,onDismiss:a}){return s({template:`
       <div class="notification-content">
         <div class="notification-header">
           <span class="notification-icon">📞</span>
