4638a0703595fbb7da55e87afa09304c038a6f88
KristinnRoach <KristinnRoach@users.noreply.github.com>
2026-02-03 17:23:54 +0000

    deploy: 466a95947355d20fa812fb29d7ee120479e12379

--- assets/push-unsupported-notification-hcnR_wPg.js
+++ assets/push-unsupported-notification-hcnR_wPg.js
@@ -1,4 +1,4 @@
-import{i,c as o}from"./main-Hd_BIeW8.js";const t="push-unsupported";function a(){if(i.has(t))return i.notifications.get(t);const n=o({template:`
+import{i,c as o}from"./main-BfO875ZX.js";const t="push-unsupported";function a(){if(i.has(t))return i.notifications.get(t);const n=o({template:`
       <div class="notification-content">
         <div class="notification-header">
           <span class="notification-title">Push notifications unavailable</span>
