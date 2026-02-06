aa2d9b41245aed9eb850a8197897a2e10ca363a6
kiddi <kristinnroach@gmail.com>
2026-02-06 21:42:30 +0000

    Updates

--- assets/push-unsupported-notification-DkP1aKai.js
+++ assets/push-unsupported-notification-DkP1aKai.js
@@ -1,4 +1,4 @@
-import{i,c as o}from"./main-Ci6PTdON.js";const t="push-unsupported";function a(){if(i.has(t))return i.notifications.get(t);const n=o({template:`
+import{i,c as o}from"./main-BZNf6BPR.js";const t="push-unsupported";function a(){if(i.has(t))return i.notifications.get(t);const n=o({template:`
       <div class="notification-content">
         <div class="notification-header">
           <span class="notification-title">Push notifications unavailable</span>
