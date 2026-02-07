78611d2c65fa08623324907f6c3cfa429c2bef36
KristinnRoach <KristinnRoach@users.noreply.github.com>
2026-02-07 21:32:27 +0000

    deploy: 9141e7d37b6a1ad216d4358a45b067babac4bceb

--- assets/push-unsupported-notification-y5slyu3n.js
+++ assets/push-unsupported-notification-y5slyu3n.js
@@ -1,4 +1,4 @@
-import{i,c as o}from"./main-C5Ext9UX.js";const t="push-unsupported";function a(){if(i.has(t))return i.notifications.get(t);const n=o({template:`
+import{i,c as o}from"./main-Br7fdOpe.js";const t="push-unsupported";function a(){if(i.has(t))return i.notifications.get(t);const n=o({template:`
       <div class="notification-content">
         <div class="notification-header">
           <span class="notification-title">Push notifications unavailable</span>
