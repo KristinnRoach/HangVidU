6c5c209bf24017828d8e4a51df28b3043deb97fa
KristinnRoach <KristinnRoach@users.noreply.github.com>
2026-02-07 01:24:52 +0000

    deploy: d356b7855072d8c89ff4979bee20e4eda28e5a04

--- assets/push-unsupported-notification-2s6shugq.js
+++ assets/push-unsupported-notification-2s6shugq.js
@@ -1,4 +1,4 @@
-import{i,c as o}from"./main-DO252Zke.js";const t="push-unsupported";function a(){if(i.has(t))return i.notifications.get(t);const n=o({template:`
+import{i,c as o}from"./main-DzxNJMuo.js";const t="push-unsupported";function a(){if(i.has(t))return i.notifications.get(t);const n=o({template:`
       <div class="notification-content">
         <div class="notification-header">
           <span class="notification-title">Push notifications unavailable</span>
