c81de39dd92e29d15e881597783fa9a57cd3be8a
kiddi <kristinnroach@gmail.com>
2026-02-19 22:17:36 +0000

    decouple call controller and messages ui

--- src/ui/bind-call-ui.js
+++ src/ui/bind-call-ui.js
@@ -2,12 +2,20 @@
 
 import { onCallConnected, onCallDisconnected } from './call-lifecycle-ui.js';
 import { hideCallingUI } from './components/calling/calling-ui.js';
+import { messagesUI } from './components/messages/messages-ui.js';
 
 /** Wires UI-only handlers to CallController. Business logic handlers are in main.js. */
 export function bindCallUI(CallController) {
   CallController.on('memberJoined', onCallConnected);
+
+  CallController.on('fileTransportReady', ({ controller }) => {
+    messagesUI.setFileTransferController(controller);
+  });
+
   CallController.on('cleanup', () => {
     hideCallingUI();
+    messagesUI.setFileTransferController(null);
+    messagesUI.reset();
     onCallDisconnected();
   });
 }
