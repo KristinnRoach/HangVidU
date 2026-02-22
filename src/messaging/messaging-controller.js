586f21bd7784455a6765f8e8925c56ae75cb5bd2
kiddi <kristinnroach@gmail.com>
2026-02-22 22:18:22 +0000

    delete commented out code

--- src/messaging/messaging-controller.js
+++ src/messaging/messaging-controller.js
@@ -1,7 +1,6 @@
 // src/messaging/messaging-controller.js
 // Unified messaging API with transport abstraction
 
-import { getContactByMostRecentInteraction } from '../ui/components/contacts/contacts.js';
 import { RTDBMessagingTransport } from './transports/rtdb-transport.js';
 
 /**
@@ -32,16 +31,6 @@ export class MessagingController {
 
     this.transport = transport;
     this.sessions = new Map(); // contactId -> session object
-
-    //     getContactByMostRecentInteraction().then((contact) => {
-    //   if (contact) {
-    //     console.info(
-    //       `[MessagingController] Most recent contact: ${contact.name} (${contact.id})`,
-    //     );
-    //   } else {
-    //     console.info('[MessagingController] No recent contacts found');
-    //   }
-    // });
   }
 
   /**
