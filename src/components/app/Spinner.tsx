596aec5a4029cc0e2bd97db4081d8e4b21013176
kiddi <kristinnroach@gmail.com>
2026-07-02 16:02:58 +0000

    Use CSS animation for loading spinner

--- src/components/app/Spinner.tsx
+++ src/components/app/Spinner.tsx
@@ -7,6 +7,7 @@ const DEFAULT_SIZE = 64;
 export function Spinner(props: SpinnerProps) {
   return (
     <svg
+      class="spinner"
       width={props.size ?? DEFAULT_SIZE}
       height={props.size ?? DEFAULT_SIZE}
       viewBox="0 0 50 50"
@@ -22,16 +23,7 @@ export function Spinner(props: SpinnerProps) {
         stroke-width="4"
         stroke-linecap="round"
         stroke-dasharray="80 40"
-      >
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          from="0 25 25"
-          to="360 25 25"
-          dur="1s"
-          repeatCount="indefinite"
-        />
-      </circle>
+      />
     </svg>
   );
 }
