afd182c7b9de9858a71940d80a0c76300b45d05c
kiddi <kristinnroach@gmail.com>
2026-07-03 23:14:32 +0000

    tailwind v4 nitpicks

--- src/components/app/LocaleToggle.tsx
+++ src/components/app/LocaleToggle.tsx
@@ -11,7 +11,7 @@ export default function LocaleToggle() {
   return (
     <button
       id="toggle-lang-btn"
-      class="absolute bottom-1 left-2 z-[200] cursor-pointer px-2.5 py-[5px] text-xs font-bold whitespace-nowrap text-neutral-300
+      class="absolute bottom-1 left-2 z-200 cursor-pointer px-2.5 py-1.25 text-xs font-bold whitespace-nowrap text-neutral-300
         hover:border-primary hover:bg-transparent hover:text-neutral-200 hover:shadow-[0_2px_8px_rgb(0_0_0/25%)] hover:outline-1 hover:outline-primary-subtle hover:outline-offset-2
         focus-visible:border-primary focus-visible:bg-transparent focus-visible:text-neutral-200 focus-visible:shadow-[0_2px_8px_rgb(0_0_0/25%)] focus-visible:outline-1 focus-visible:outline-primary-subtle focus-visible:outline-offset-2"
       type="button"
